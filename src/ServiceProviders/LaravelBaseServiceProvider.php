655f7a3da9c6b5cd25c2868a323f4a021d6fab23
Gonçalo Queirós <mail@goncaloqueiros.net>
2014-03-24 19:34:20 +0000

    Initial commit

---
+++ src/ServiceProviders/LaravelBaseServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ghunti\LaravelBase\ServiceProviders;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBaseServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+}
